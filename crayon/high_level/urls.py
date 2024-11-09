369f6b45e2cfa9f6fdb33e1dc4c82bd959351247
Souf2000 <soufianelahlali506@gmail.com>
2024-11-09 19:32:03 +0100

    Commit 9 nov

--- crayon/high_level/urls.py
+++ crayon/high_level/urls.py
@@ -3,13 +3,22 @@ from . import views
 
 urlpatterns = [
     path('villes/', views.ville_list, name='ville-list'),
-    path('machines/', views.machine_list, name='machine-list'),
-    path('usines/', views.usine_list, name='usine-list'),
+    path('villes/<int:id>/', views.ville_detail, name='ville-detail'),
     path('ressources/', views.ressource_list, name='ressource-list'),
+    path('ressources/<int:id>/', views.ressource_detail, name='ressource-detail'),
+    path('machines/', views.machine_list, name='machine-list'),
+    path('machines/<int:id>/', views.machine_detail, name='machine-detail'),
+    path('usines/', views.usine_list, name='usine-list'),
+    path('usines/<int:id>/', views.usine_detail, name='usine-detail'),
     path('stocks/', views.stock_list, name='stock-list'),
-    path('quantite_ressources/', views.quantiteressource_list, name='quantiteressource-list'),
+    path('stocks/<int:id>/', views.stock_detail, name='stock-detail'),
+    path('quantite-ressources/', views.quantite_ressource_list, name='quantite-ressource-list'),
+    path('quantite-ressources/<int:id>/', views.quantite_ressource_detail, name='quantite-ressource-detail'),
     path('etapes/', views.etape_list, name='etape-list'),
+    path('etapes/<int:id>/', views.etape_detail, name='etape-detail'),
     path('produits/', views.produit_list, name='produit-list'),
+    path('produits/<int:id>/', views.produit_detail, name='produit-detail'),
     path('recettes/', views.recette_list, name='recette-list'),
+    path('recettes/<int:id>/', views.recette_detail, name='recette-detail'),
 ]
 
--- crayon/high_level/views.py
+++ crayon/high_level/views.py
@@ -1,39 +1,129 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from .models import Ville, Machine, Usine, Ressource, Stock, QuantiteRessource, Etape, Produit, Recette
 
+# Vue pour lister toutes les villes
 def ville_list(request):
-    villes = [ville.json() for ville in Ville.objects.all()]
-    return JsonResponse(villes, safe=False)
+    villes = Ville.objects.all()
+    data = [ville.json() for ville in villes]
+    return JsonResponse(data, safe=False)
 
-def machine_list(request):
-    machines = [machine.json() for machine in Machine.objects.all()]
-    return JsonResponse(machines, safe=False)
+# Vue pour afficher les détails d'une ville par ID
+def ville_detail(request, id):
+    try:
+        ville = Ville.objects.get(id=id)
+        return JsonResponse(ville.json())
+    except Ville.DoesNotExist:
+        raise Http404
 
-def usine_list(request):
-    usines = [usine.json() for usine in Usine.objects.all()]
-    return JsonResponse(usines, safe=False)
-
+# Vue pour lister toutes les ressources
 def ressource_list(request):
-    ressources = [ressource.json() for ressource in Ressource.objects.all()]
-    return JsonResponse(ressources, safe=False)
+    ressources = Ressource.objects.all()
+    data = [ressource.json() for ressource in ressources]
+    return JsonResponse(data, safe=False)
 
+# Vue pour afficher les détails d'une ressource par ID
+def ressource_detail(request, id):
+    try:
+        ressource = Ressource.objects.get(id=id)
+        return JsonResponse(ressource.json())
+    except Ressource.DoesNotExist:
+        raise Http404
+
+# Vue pour lister toutes les machines
+def machine_list(request):
+    machines = Machine.objects.all()
+    data = [machine.json() for machine in machines]
+    return JsonResponse(data, safe=False)
+
+# Vue pour afficher les détails d'une machine par ID
+def machine_detail(request, id):
+    try:
+        machine = Machine.objects.get(id=id)
+        return JsonResponse(machine.json())
+    except Machine.DoesNotExist:
+        raise Http404
+
+# Vue pour lister toutes les usines
+def usine_list(request):
+    usines = Usine.objects.all()
+    data = [usine.json() for usine in usines]
+    return JsonResponse(data, safe=False)
+
+# Vue pour afficher les détails d'une usine par ID
+def usine_detail(request, id):
+    try:
+        usine = Usine.objects.get(id=id)
+        return JsonResponse(usine.json())
+    except Usine.DoesNotExist:
+        raise Http404
+
+# Vue pour lister tous les stocks
 def stock_list(request):
-    stocks = [stock.json() for stock in Stock.objects.all()]
-    return JsonResponse(stocks, safe=False)
+    stocks = Stock.objects.all()
+    data = [stock.json() for stock in stocks]
+    return JsonResponse(data, safe=False)
 
-def quantiteressource_list(request):
-    quantite_ressources = [qr.json() for qr in QuantiteRessource.objects.all()]
-    return JsonResponse(quantite_ressources, safe=False)
+# Vue pour afficher les détails d'un stock par ID
+def stock_detail(request, id):
+    try:
+        stock = Stock.objects.get(id=id)
+        return JsonResponse(stock.json())
+    except Stock.DoesNotExist:
+        raise Http404
 
+# Vue pour lister toutes les quantités de ressources
+def quantite_ressource_list(request):
+    quantites = QuantiteRessource.objects.all()
+    data = [quantite.json() for quantite in quantites]
+    return JsonResponse(data, safe=False)
+
+# Vue pour afficher les détails d'une quantité de ressource par ID
+def quantite_ressource_detail(request, id):
+    try:
+        quantite = QuantiteRessource.objects.get(id=id)
+        return JsonResponse(quantite.json())
+    except QuantiteRessource.DoesNotExist:
+        raise Http404
+
+# Vue pour lister toutes les étapes
 def etape_list(request):
-    etapes = [etape.json() for etape in Etape.objects.all()]
-    return JsonResponse(etapes, safe=False)
+    etapes = Etape.objects.all()
+    data = [etape.json() for etape in etapes]
+    return JsonResponse(data, safe=False)
 
+# Vue pour afficher les détails d'une étape par ID
+def etape_detail(request, id):
+    try:
+        etape = Etape.objects.get(id=id)
+        return JsonResponse(etape.json())
+    except Etape.DoesNotExist:
+        raise Http404
+
+# Vue pour lister tous les produits
 def produit_list(request):
-    produits = [produit.json() for produit in Produit.objects.all()]
-    return JsonResponse(produits, safe=False)
+    produits = Produit.objects.all()
+    data = [produit.json() for produit in produits]
+    return JsonResponse(data, safe=False)
 
+# Vue pour afficher les détails d'un produit par ID
+def produit_detail(request, id):
+    try:
+        produit = Produit.objects.get(id=id)
+        return JsonResponse(produit.json())
+    except Produit.DoesNotExist:
+        raise Http404
+
+# Vue pour lister toutes les recettes
 def recette_list(request):
-    recettes = [recette.json() for recette in Recette.objects.all()]
-    return JsonResponse(recettes, safe=False)
+    recettes = Recette.objects.all()
+    data = [recette.json() for recette in recettes]
+    return JsonResponse(data, safe=False)
+
+# Vue pour afficher les détails d'une recette par ID
+def recette_detail(request, id):
+    try:
+        recette = Recette.objects.get(id=id)
+        return JsonResponse(recette.json())
+    except Recette.DoesNotExist:
+        raise Http404
 
